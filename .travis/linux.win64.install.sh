41833627db52f5d92bf4bd9a432310ec8c31d575
curlymorphic <dave.french3@googlemail.com>
2015-04-27 19:04:47 +0100

    added mingw64 dependancys to travis

--- .travis/linux.win64.install.sh
+++ .travis/linux.win64.install.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y mingw64-x-qt mingw64-x-sdl mingw64-x-libvorbis			\
 	mingw64-x-libsndfile mingw64-x-fftw mingw64-x-flac mingw64-x-fltk			\
 	mingw64-x-libsamplerate mingw64-x-pkgconfig mingw64-x-binutils mingw64-x-gcc\
 	mingw64-x-runtime mingw64-x-libgig \
-        mingw64-x-rtosc mingw-x-liblo
+        mingw64-x-rtosc mingw64-x-liblo
