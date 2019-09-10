c71413fc217caecbbe56de39c9e818e953387de9
Ian Sannar <8443754+iansannar@users.noreply.github.com>
2019-09-10 08:29:06 -0600

    :penguin: Fix wildcard, improve readability

--- cmake/branding/generate_icons_linux.sh
+++ cmake/branding/generate_icons_linux.sh
@@ -5,19 +5,19 @@ echo Recoloring vector icon...
 case "$1" in
 	"-r" ) # Release
 	sed 's/#780116/#249a56/g; s/#c51306/#50d99b/g' icon.svg > recolored.svg
-	sed 's/#780116/#249a56/g; s/#c51306/#50d99b/g' splash.svg > splash-recolored.svg
+	sed 's/#780116/#249a56/g; s/#c51306/#50d99b/g' splash.svg > recolored-splash.svg
 	;;
 	"-b" | "-rc" ) # Beta/Release Candidate
 	sed 's/#780116/#0267c1/g; s/#c51306/#09a9d9/g' icon.svg > recolored.svg
-	sed 's/#780116/#0267c1/g; s/#c51306/#09a9d9/g' splash.svg > splash-recolored.svg
+	sed 's/#780116/#0267c1/g; s/#c51306/#09a9d9/g' splash.svg > recolored-splash.svg
 	;;
 	"-c" | "-n" ) # Canary/Nightly
 	sed 's/#780116/#ffa40f/g; s/#c51306/#fad200/g' icon.svg > recolored.svg
-	sed 's/#780116/#ffa40f/g; s/#c51306/#fad200/g' splash.svg > splash-recolored.svg
+	sed 's/#780116/#ffa40f/g; s/#c51306/#fad200/g' splash.svg > recolored-splash.svg
 	;;
 	* ) # No type specified, leave it be
 	sed '' icon.svg > recolored.svg
-	sed '' splash.svg > splash-recolored.svg
+	sed '' splash.svg > recolored-splash.svg
 	;;
 esac
 
@@ -26,17 +26,17 @@ echo Generating raster images for icon...
 for RES in 16 24 32 48 64 96 128
 do
 	mkdir -p icons/${RES}x${RES}/apps
-	rsvg-convert -w $RES -h $RES -o "icons/${RES}x${RES}/apps/lmms.png" recolored.svg
+	rsvg-convert -w $RES -h $RES recolored.svg -o "icons/${RES}x${RES}/apps/lmms.png"
 done
 
 for RES in 16 24 32 48 64 96 128
 do
 	mkdir -p icons/${RES}x${RES}@2/apps
-	rsvg-convert -w $((RES * 2)) -h $((RES * 2)) -o "icons/${RES}x${RES}@2/apps/lmms.png" recolored.svg
+	rsvg-convert -w $((RES * 2)) -h $((RES * 2)) recolored.svg -o "icons/${RES}x${RES}@2/apps/lmms.png"
 done
 
 echo Generating raster image for splash screen...
-rsvg-convert -w 680 -h 573 -o "../../data/themes/default/splash.png" splash-recolored.svg
+rsvg-convert -w 680 -h 573 recolored-splash.svg -o "../../data/themes/default/splash.png"
 
 mkdir -p icons/scalable/apps
 cp recolored.svg icons/scalable/apps/lmms.svg
@@ -46,5 +46,5 @@ cp -r -f icons ../linux/
 
 # Clean up images
 echo Cleaning up temporary images...
-rm -f *recolored.svg
+rm -f recolored*.svg
 rm -rf icons
