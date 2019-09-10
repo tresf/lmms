c696ce85a05b35146f29a442c70c0bac7bd2acdd
Ian Sannar <8443754+iansannar@users.noreply.github.com>
2019-09-10 08:31:42 -0600

    :apple: Fix wildcard, improve readability

--- cmake/branding/generate_icons_osx.sh
+++ cmake/branding/generate_icons_osx.sh
@@ -5,31 +5,31 @@ echo Recoloring vector icon...
 case "$1" in
 	"-r" ) # Release
 	sed 's/#780116/#249a56/g; s/#c51306/#50d99b/g' icon.svg > recolored.svg
-	sed 's/#780116/#249a56/g; s/#c51306/#50d99b/g' splash.svg > splash-recolored.svg
+	sed 's/#780116/#249a56/g; s/#c51306/#50d99b/g' splash.svg > recolored-splash.svg
 	;;
 	"-b" | "-rc" ) # Beta/Release Candidate
 	sed 's/#780116/#0267c1/g; s/#c51306/#09a9d9/g' icon.svg > recolored.svg
-	sed 's/#780116/#0267c1/g; s/#c51306/#09a9d9/g' splash.svg > splash-recolored.svg
+	sed 's/#780116/#0267c1/g; s/#c51306/#09a9d9/g' splash.svg > recolored-splash.svg
 	;;
 	"-c" | "-n" ) # Canary/Nightly
 	sed 's/#780116/#ffa40f/g; s/#c51306/#fad200/g' icon.svg > recolored.svg
-	sed 's/#780116/#ffa40f/g; s/#c51306/#fad200/g' splash.svg > splash-recolored.svg
+	sed 's/#780116/#ffa40f/g; s/#c51306/#fad200/g' splash.svg > recolored-splash.svg
 	;;
 	* ) # No type specified, leave it be
 	sed '' icon.svg > recolored.svg
-	sed '' splash.svg > splash-recolored.svg
+	sed '' splash.svg > recolored-splash.svg
 	;;
 esac
 
 # Generate raster images
 echo Generating raster images for icon...
-rsvg-convert -w 1024 -h 1024 icon_512x512@2x.png recolored.svg
-rsvg-convert -w 512 -h 512 -o icon_512x512.png recolored.svg
-rsvg-convert -w 256 -h 256 -o icon_256x256.png recolored.svg
-rsvg-convert -w 128 -h 128 -o icon_128x128.png recolored.svg
-rsvg-convert -w 64 -h 64 -o icon_64x64.png recolored.svg
-rsvg-convert -w 32 -h 32 -o icon_32x32.png recolored.svg
-rsvg-convert -w 16 -h 16 -o icon_16x16.png recolored.svg
+rsvg-convert -w 1024 -h 1024 recolored.svg -o icon_512x512@2x.png
+rsvg-convert -w 512 -h 512 recolored.svg -o icon_512x512.png
+rsvg-convert -w 256 -h 256 recolored.svg -o icon_256x256.png
+rsvg-convert -w 128 -h 128 recolored.svg -o icon_128x128.png
+rsvg-convert -w 64 -h 64 recolored.svg -o icon_64x64.png
+rsvg-convert -w 32 -h 32 recolored.svg -o icon_32x32.png
+rsvg-convert -w 16 -h 16 recolored.svg -o icon_16x16.png
 
 # Move to .iconset
 mkdir lmms.iconset
@@ -46,10 +46,10 @@ echo Generating macOS .icns from raster images...
 iconutil -c icns -o icon.icns lmms.iconset
 
 echo Generating raster image for splash screen...
-rsvg-convert -w 680 -h 573 -o "../../data/themes/default/splash.png" splash-recolored.svg
+rsvg-convert -w 680 -h 573 recolored-splash.svg -o "../../data/themes/default/splash.png"
 
 # Clean up images
 echo Cleaning up temporary images...
-rm -f *recolored.svg
+rm -f recolored*.svg
 rm -f icon_*.png
 rm -r lmms.iconset
