3b372450bba259a2083724ab55396ad958986dd5
tresf <tres.finocchiaro@gmail.com>
2020-01-10 14:44:59 -0500

    Fix Carla detection on Linux Per #5369

--- cmake/linux/launch_lmms.sh
+++ cmake/linux/launch_lmms.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-alias standard_which="command -v"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH="$PATH:/sbin"
-if standard_which carla > /dev/null 2>&1; then
+if command -v carla > /dev/null 2>&1; then
    CARLAPATH="$(standard_which carla)"
    CARLAPREFIX="${CARLAPATH%/bin*}"
    echo "Carla appears to be installed on this system at $CARLAPREFIX/lib[64]/carla so we'll use it."
