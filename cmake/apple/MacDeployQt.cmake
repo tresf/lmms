69ad41e7f246eb4e20815a105c74ef7d0497d62b
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2025-02-07 02:40:31 -0500

    Add Windows ico support

--- cmake/apple/MacDeployQt.cmake
+++ cmake/apple/MacDeployQt.cmake
@@ -67,8 +67,13 @@ list(REVERSE mime_parts)
 list(JOIN mime_parts "." MACOS_MIMETYPE_ID)
 configure_file("${CPACK_CURRENT_SOURCE_DIR}/lmms.plist.in" "${APP}/Contents/Info.plist" @ONLY)
 file(COPY "${CPACK_CURRENT_SOURCE_DIR}/project.icns" DESTINATION "${APP}/Contents/Resources")
-file(COPY "${CPACK_CURRENT_SOURCE_DIR}/icon.icns" DESTINATION "${APP}/Contents/Resources")
-file(RENAME "${APP}/Contents/Resources/icon.icns" "${APP}/Contents/Resources/${lmms}.icns")
+
+# Branding override
+if(EXISTS "${CPACK_CURRENT_BINARY_DIR}/${lmms}.icns")
+	file(COPY "${CPACK_CURRENT_BINARY_DIR}/${lmms}.icns" DESTINATION "${APP}/Contents/Resources")
+else()
+	file(COPY "${CPACK_CURRENT_SOURCE_DIR}/${lmms}.icns" DESTINATION "${APP}/Contents/Resources")
+endif()
 
 # Copy Suil modules
 if(CPACK_SUIL_MODULES)
